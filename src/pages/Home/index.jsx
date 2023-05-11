e9706f7324b76d0932c2290e0df96bde064283ef
matheusnoal <matheusnoal2@gmail.com>
2023-05-10 22:11:00 -0300

    home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,46 @@
+import { FiPlus } from 'react-icons/fi'
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { Section } from '../../components/Section'
+import { Note } from '../../components/Note'
+import { ButtonText } from '../../components/ButtonText'
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>Rocketnotes</h1>
+      </Brand>
+
+      <Header />
+
+      <Menu>
+        <li><ButtonText title="Todos" /></li>
+        <li><ButtonText title="React" isActive /></li>
+        <li><ButtonText title="Node" /></li>
+      </Menu>
+
+      <Search>
+        <Input placeholder="Pesquisar pelo título" />
+      </Search>
+
+      <Content>
+        <Section title="Minhas notas">
+          <Note data={{
+            title: 'React',
+            tags: [
+              { id: '1', name: 'react' },
+              { id: '2', name: 'rocketseat' }
+            ]
+          }} />
+        </Section>
+      </Content>
+
+      <NewNote>
+        <FiPlus />
+        Criar nota
+      </NewNote>
+    </Container>
+  );
+}
