4ce89421cc46ee8402089468c930b4123280f311
matheusnoal <matheusnoal2@gmail.com>
2023-05-13 15:25:04 -0300

    routes

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -2,6 +2,7 @@ import { Container, Form, Background } from './styles'
 import { FiMail, FiLock } from 'react-icons/fi'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { Link } from 'react-router-dom'
 
 export function SignIn() {
   return (
@@ -26,7 +27,7 @@ export function SignIn() {
 
         <Button title="Entrar" />
 
-        <a href="#">Criar conta</a>
+        <Link to="/register" >Criar conta</Link>
       </Form>
 
       <Background />
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,6 +2,7 @@ import { Container, Form, Background } from './styles'
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { Link } from 'react-router-dom'
 
 export function SignUp() {
   return (
@@ -14,12 +15,12 @@ export function SignUp() {
 
         <h2>Crie sua conta</h2>
 
-        <Input 
+        <Input
           placeholder="Nome"
           type="text"
           icon={FiUser}
         />
-        
+
         <Input
           placeholder="E-mail"
           type="text"
@@ -34,7 +35,7 @@ export function SignUp() {
 
         <Button title="Cadastrar" />
 
-        <a href="#">Voltar para o login</a>
+        <Link to="/" >Voltar para o login</Link>
       </Form>
 
     </Container>
